daf68cbd3dac1adb6ece862645299a6e0c02a83b
RiPoppe <rickpoppe3@gmail.com>
2021-10-13 16:17:37 +0200

    tweede commit

---
+++ DeployApp/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeployApp.Controllers
+{
+    [Route("Shuttle")]
+    [ApiController]
+    public class TestController : Controller
+    {
+        // GET: /Shuttle
+        [HttpGet]
+        public string HelloWorld()
+        {
+            return "Hello World!";
+        }
+    }
+}
